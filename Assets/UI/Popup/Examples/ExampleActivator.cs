7d7d4d5150ee508cd77087a195b53ceab016e2bc
Gab <160644362+rznn7@users.noreply.github.com>
2024-05-13 23:12:20 +0200

    Popup message manager

---
+++ Assets/UI/Popup/Examples/ExampleActivator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class ExampleActivator : MonoBehaviour, IPopupActivator
+{
+    public event Action ActivatePopup;
+
+    public void SendActivationEvent()
+    {
+        ActivatePopup?.Invoke();
+        Debug.Log("Clicked on message button in cockpit");
+    }
+}
