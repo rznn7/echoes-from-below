868729cc14a1c65c690a618b0d1801be4a555820
Gab <160644362+rznn7@users.noreply.github.com>
2024-05-16 21:59:39 +0200

    Text interaction System

---
+++ Assets/UI/TextInteraction/Scripts/TextualInteractionEndUIElement.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TextualInteractionEndUIElement : MonoBehaviour
+{
+    UIDocument _uiDocument;
+
+    Label _interactionEndText;
+    Button _interactionEndButton;
+
+    public event Action EndInteraction;
+
+    void Awake()
+    {
+        _uiDocument = GetComponent<UIDocument>();
+
+        InitUIElements();
+        SubscribeToButtonClicks();
+    }
+
+    void OnDestroy()
+    {
+        UnsubscribeFromButtonsClick();
+    }
+
+    void InitUIElements()
+    {
+        var root = _uiDocument.rootVisualElement;
+
+        _interactionEndText = root.Q<Label>("interactionEndText");
+        _interactionEndButton = root.Q<Button>("interactionEndButton");
+    }
+
+    void SubscribeToButtonClicks()
+    {
+        _interactionEndButton.clicked += OnInteractionEndButtonClicked;
+    }
+
+    void UnsubscribeFromButtonsClick()
+    {
+        _interactionEndButton.clicked -= OnInteractionEndButtonClicked;
+    }
+
+    void OnInteractionEndButtonClicked()
+    {
+        EndInteraction?.Invoke();
+    }
+
+    public void Show()
+    {
+        _uiDocument.rootVisualElement.style.display = DisplayStyle.Flex;
+    }
+
+    public void Hide()
+    {
+        _uiDocument.rootVisualElement.style.display = DisplayStyle.None;
+    }
+
+    public void SetData(TextualInteraction interaction)
+    {
+        _interactionEndText.text = interaction.InteractionEndText;
+        _interactionEndButton.text = interaction.InteractionEndButtonText;
+    }
+}
