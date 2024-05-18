35fa0fea97ebaf4bde72036e66fca4321fc731ce
Reshad <reshad4587@gmail.com>
2024-05-18 13:45:03 -0400

    Fixed shipwreck detection by player

--- Assets/Scripts/SubmarineComponents/Shipwreck.cs
+++ Assets/Scripts/SubmarineComponents/Shipwreck.cs
@@ -5,47 +5,51 @@ using Random = UnityEngine.Random;
 
 public class Shipwreck : MonoBehaviour
 {
-    public void DropItem()
+    public IEnumerator HandleEventInteraction()
     {
         EventInteraction interaction = FindObjectOfType<EventInteraction>();
-        
-        if (interaction == null)
-            return;
+        if (interaction == null) yield break;
 
-       
+        bool eventHandled = false;
+
+        Action onAccept = () => 
+        {
+            Debug.Log("Player accepted the interaction");
+            eventHandled = true;
+        };
+
+        Action onDeny = () =>
+        {
+            Debug.Log("Player denied the interaction");
+            eventHandled = true;
+        };
+
         if (Random.value < 0.25f)
         {
             int batteryAmountToCharge = Random.Range(10, 50);
-            interaction.SendBatteryInteractionMessage(batteryAmountToCharge, () =>
-            {
-                GameUIManager.updatePower(GameUIManager.instance.power.value + batteryAmountToCharge);
-            });
+            interaction.SendBatteryInteractionMessage(batteryAmountToCharge, onAccept, onDeny);
         }
-        else if (Random.value > 0.25f && Random.value < 0.5f)
+        else if (Random.value < 0.5f)
         {
             int oxygenAmountToRecover = Random.Range(10, 50);
-            interaction.SendOxygenInteractionMessage(oxygenAmountToRecover, () =>
-            {
-                GameUIManager.updateOxygen(GameUIManager.instance.oxygen.value + oxygenAmountToRecover);
-            });
+            interaction.SendOxygenInteractionMessage(oxygenAmountToRecover, onAccept, onDeny);
         }
-        else if (Random.value > 0.5f && Random.value < 0.75f)
+        else if (Random.value < 0.75f)
         {
             int scrapToCollect = Random.Range(1, 4);
-            interaction.SendScrapInteractionMessage(scrapToCollect, () =>
-            {
-                GameUIManager.updateScrap(FindObjectOfType<PlayerStats>().scrapCount + scrapToCollect);
-            });
+            interaction.SendScrapInteractionMessage(scrapToCollect, onAccept, onDeny);
         }
         else
         {
             int ammoToCollect = Random.Range(1, 4);
-            interaction.SendAmmoInteractionMessage(ammoToCollect, () =>
-            {
-                GameUIManager.updateBullets(GameUIManager.instance.bullets.value + ammoToCollect);
-            });
+            interaction.SendAmmoInteractionMessage(ammoToCollect, onAccept, onDeny);
         }
-        
+
+        while (!eventHandled)
+        {
+            yield return null;
+        }
+
         Destroy(gameObject);
     }
 
