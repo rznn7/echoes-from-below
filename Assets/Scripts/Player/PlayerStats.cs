7f59a9f3a5d29c27e998618ca3da125cd83c19a3
Reshad <reshad4587@gmail.com>
2024-05-18 21:37:59 -0400

    Added leak after sea mine damage and leak percentage after moving

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -8,24 +8,26 @@ public class PlayerStats : MonoBehaviour
 
     public int scrapCount;
     
-    private PlayerMovement _playerMovement;
+    private PlayerMovement playerMovement;
     
     private void Start()
     {
-        _playerMovement = GetComponent<PlayerMovement>();
+        playerMovement = GetComponent<PlayerMovement>();
         
-        _playerMovement.OnPlayerMove += SubtractPowerAfterTurn;
-        _playerMovement.OnPlayerMove += SubtractOxygenAfterTurn;
+        playerMovement.OnPlayerMove += SubtractPowerAfterTurn;
+        playerMovement.OnPlayerMove += SubtractOxygenAfterTurn;
+        playerMovement.OnPlayerMove += ApplyLeakageAfterTurn;
         
         GameUIManager.initPower(GameUIManager.instance.power.max);
         GameUIManager.initOxygen(GameUIManager.instance.oxygen.max);
-        GameUIManager.initLeak(GameUIManager.instance.leak.min);
+        GameUIManager.initLeak(GameUIManager.instance.leak.max);
     }
 
     private void OnDestroy()
     {
-        _playerMovement.OnPlayerMove -= SubtractPowerAfterTurn;
-        _playerMovement.OnPlayerMove -= SubtractOxygenAfterTurn;
+        playerMovement.OnPlayerMove -= SubtractPowerAfterTurn;
+        playerMovement.OnPlayerMove -= SubtractOxygenAfterTurn;
+        playerMovement.OnPlayerMove -= ApplyLeakageAfterTurn;
     }
 
     private void SubtractPowerAfterTurn()
@@ -36,5 +38,26 @@ public class PlayerStats : MonoBehaviour
     private void SubtractOxygenAfterTurn()
     {
         GameUIManager.updateOxygen(GameUIManager.instance.oxygen.value - oxygenToSubtract);
+    }
+    
+    private void ApplyLeakageAfterTurn()
+    {
+        float currentLeak = GameUIManager.instance.leak.value;
+        float additionalLeakage = 0f;
+
+        if (currentLeak is > 20 and <= 50)
+        {
+            additionalLeakage = currentLeak * 0.03f;
+        }
+        else if (currentLeak is > 50 and <= 70)
+        {
+            additionalLeakage = currentLeak * 0.05f;
+        }
+        else if (currentLeak > 70)
+        {
+            additionalLeakage = currentLeak * 0.07f;
+        }
+
+        GameUIManager.updateLeak(currentLeak + additionalLeakage);
     }
 }
